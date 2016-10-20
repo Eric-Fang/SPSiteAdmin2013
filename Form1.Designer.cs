32dfb53d420c184bb00aca623c5c33da5f03229b
0efang <0efang@PDBIW01>
2016-10-20 13:33:25 +1100

    allow to move site collection to different managed path

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -198,6 +198,7 @@
             this.comboBoxInWebAppManagedPath.Name = "comboBoxInWebAppManagedPath";
             this.comboBoxInWebAppManagedPath.Size = new System.Drawing.Size(350, 21);
             this.comboBoxInWebAppManagedPath.TabIndex = 44;
+            this.comboBoxInWebAppManagedPath.SelectedIndexChanged += new System.EventHandler(this.comboBoxInWebAppManagedPath_SelectedIndexChanged);
             // 
             // label4
             // 
